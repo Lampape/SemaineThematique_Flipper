429dc4d93a43ec0b6f66ea8b22eea032a2d0b613
Lampape <62471565+Lampape@users.noreply.github.com>
2020-12-01 11:28:21 +0100

    Ca c'est mes assets

---
+++ Script/PoiintGravitation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoiintGravitation : MonoBehaviour
+{
+    public Transform target;
+
+    public float multiplicationFactor = 3.0f;
+    // Update is called once per frame
+    void Update()
+    {
+          Rigidbody bradFlipper = gameObject.GetComponent<Rigidbody>(); 
+              bradFlipper.AddForce((target.position - transform.position) * multiplicationFactor); 
+    }
+}
