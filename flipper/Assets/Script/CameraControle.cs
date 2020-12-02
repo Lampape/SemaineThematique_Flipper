4dd8bcfc0e60ddd30c37bf0ab39a06d3133456a0
RaphaMartins <75305017+RaphaMartins@users.noreply.github.com>
2020-12-02 18:33:55 +0100

    Camera Controle

---
+++ flipper/Assets/Script/CameraControle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControle : MonoBehaviour
+{
+    private Vector2 mouseInput;
+    public float mouseSensitivity = 1f;
+
+    void Update()
+    {
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x,
+            transform.rotation.eulerAngles.z);
+    }
+}
