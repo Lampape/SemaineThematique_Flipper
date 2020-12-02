eb55345df3eb22ef41b721b7e502db3ca353f7bf
Gonzalo Bourdieu <gbourdieu@myges.fr>
2020-12-02 16:43:16 +0100

    Accel+stop+target

---
+++ flipper/Assets/Script/Accel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accel : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float force = 1.0f;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        rb = other.GetComponent<Rigidbody>();
+
+        Vector3 dir = transform.right;
+        rb.AddForce(dir * force, ForceMode.Impulse);
+
+
+    }
+}
