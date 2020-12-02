1550c83c273bbba9db448b7c4408fd394399a5f1
Gonzalo Bourdieu <gbourdieu@myges.fr>
2020-12-02 11:12:52 +0100

    Version 0.1 proto

---
+++ flipper/Assets/Script/BombePoing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombePoing : MonoBehaviour
+{
+    public Rigidbody Bille;   
+    public float radius = 10.0F;
+    public float power = 10.0F;
+    public float upwardModifier = 1f;
+
+    private void Start()
+    {
+        //Bille = GetComponent<Rigidbody>()
+        Bille.AddExplosionForce(power, transform.position, radius, upwardModifier,
+                    ForceMode.Impulse);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    
+}
---
+++ flipper/Assets/Script/FulguroPoing.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FulguroPoing : MonoBehaviour
+{
+    public GameObject Droite;
+    public GameObject Gauche;
+    public GameObject laBombe;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+        
+              GameObject delayObject = Instantiate(laBombe, Gauche.transform.position, Quaternion.identity);
+              Destroy(delayObject,1.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GameObject delayObject = Instantiate(laBombe, Droite.transform.position, Quaternion.identity);
+            Destroy(delayObject,1.0f);
+        }
+    }
+}
+
+/*
+private void OnCollisionEnter(Collision collision)
+{
+if (collision.collider.CompareTag("Ground"))
+{
+    Debug.Log("Boom");
+    Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+    Debug.Log(colliders.Length);
+
+
+
+
+    foreach (Collider hit in colliders)
+    {
+        Rigidbody rb = hit.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddExplosionForce(power, transform.position, radius, upwardModifier,
+                ForceMode.Impulse);
+        }
+    }
+
+}
+
+Destroy(gameObject);
+}
+
+private void OnDrawGizmos()
+{
+Gizmos.DrawWireSphere(transform.position, radius);
+}
+}
+*/
+
