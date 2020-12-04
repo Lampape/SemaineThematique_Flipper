0209097901caac1c8191f83b67d4419e1c83c089
Lampape <62471565+Lampape@users.noreply.github.com>
2020-12-04 20:15:41 +0100

    Accel Porf modif

--- flipper/Assets/Script/Accel.cs
+++ flipper/Assets/Script/Accel.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Accel : MonoBehaviour
 {
+    private static bool ImpulseGiven ;
+    private static int colliderEnterCount ;
+    
+    
     private Rigidbody rb;
     public float force = 2.0f; //force du renvoi de la bille
     public float steerPower = 1f; //détermine à quel point la trajectoire de la bille est modifiée vers la gauche ou la droite du joueur
+    public float velocityfloat;
+    public float combosVelocityForce;
+    public int gainVelocity = 0; // valeur qui donne la velocité a la balle et l'effet combos
 
     private void OnTriggerStay(Collider other)
     {
-        Debug.Log("Collision en cours");
-
         rb = other.GetComponent<Rigidbody>();
 
         Vector3 dirDroite = transform.forward + transform.right * steerPower;
@@ -19,19 +24,50 @@ public class Accel : MonoBehaviour
         //Vector3 dirFace = transform.forward;
         //rb.AddForce(dir * force, ForceMode.Impulse);
 
+        if (Input.GetKeyDown(KeyCode.A) && !ImpulseGiven)
+        {
+            rb.AddForce(dirGauche * combosVelocityForce, ForceMode.Impulse);
+            ImpulseGiven = true;
+            Billevelocity.velocityScore += gainVelocity;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z) && !ImpulseGiven)
+        {
+            rb.AddForce(dirDroite * combosVelocityForce, ForceMode.Impulse);
+            ImpulseGiven = true;
+            Billevelocity.velocityScore += gainVelocity;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        ImpulseGiven = false;
+        colliderEnterCount--;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        colliderEnterCount++;
+       /*Debug.Log("Collision en cours");
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(dirGauche * force, ForceMode.Impulse);
-            Debug.Log("renvoi à gauche");
+            Billevelocity.velocityScore += gainVelocity;
         }
 
         if (Input.GetKey(KeyCode.Z))
         {
-            rb.AddForce(dirDroite * force, ForceMode.Impulse);
-            Debug.Log("renvoi à droite");
-        }
-
-        
-
+            Billevelocity.velocityScore += gainVelocity;
+        }*/
     }
+
+void Update()
+    {
+        if(colliderEnterCount == 0)
+        {
+            ImpulseGiven = false;
+        }
+        combosVelocityForce = force + velocityfloat;
+        velocityfloat = Billevelocity.velocityScore;
+    }
+
+
 }
---
+++ flipper/Assets/Script/Billevelocity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billevelocity : MonoBehaviour
+{
+    public static int velocityScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocityScore = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.T))
+        {
+            Debug.Log("velocity est a " + velocityScore);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            velocityScore = 0;
+            Debug.Log("ici la balle a collisioner avec player est la velo est a zero");
+        }
+    }
+}
+
+
+
