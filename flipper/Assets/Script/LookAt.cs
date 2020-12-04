6d3a19bb49187bab47bde1b1f329bef58d4fbe6e
Gonzalo Bourdieu <74542385+Elbourdy@users.noreply.github.com>
2020-12-04 17:56:26 +0100

    Balle + sprite + look at

---
+++ flipper/Assets/Script/LookAt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform targetCamera;
+    public static int velocityScore;
+
+    void Update()
+    {
+        transform.LookAt(targetCamera);
+    }
+    
+
+}
