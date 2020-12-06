f99de9809873230cb597ab2dc803b90f4827ec8d
Lampape <62471565+Lampape@users.noreply.github.com>
2020-12-06 12:25:07 +0100

    Scoring

--- flipper/Assets/Script/KillEnemie.cs
+++ flipper/Assets/Script/KillEnemie.cs
@@ -2,15 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class KillEnemie : MonoBehaviour
 {
+    public int scoreGiven = 5;
+    public int scoreTotal;
+    private int killCount;
+    public Text scoreText;
+    public Text combosText;
+
+    public void Update()
+    {
+        scoreTotal = scoreGiven * killCount * Billevelocity.velocityScore;
+        scoreText.text = "score : " + scoreTotal;
+        combosText.text = "combos : " + Billevelocity.velocityScore;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("BilleFlipper"))
         {
+            killCount++;
             Destroy(other.gameObject);
         }
     }
 }
-
