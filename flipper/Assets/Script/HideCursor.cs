7b17ff04fed1a4743d7cb6ce15f5be133fb67f91
RaphaMartins <75305017+RaphaMartins@users.noreply.github.com>
2020-12-06 22:24:14 +0100

    Cachage du cursor et correction son game over

---
+++ flipper/Assets/Script/HideCursor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class HideCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
